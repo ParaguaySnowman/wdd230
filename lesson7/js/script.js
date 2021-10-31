12d574aa0e920de60672bebd8a7bd3127cea17b3
ParaguaySnowman <parker.b.mortensen@gmail.com>
2021-10-30 20:44:06 -0700

    days since last visit first attempt

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -27,3 +27,30 @@ if('IntersectionObserver' in window) {
       loadImages(img);
     });
   }
+
+
+//To display days passed since last vist
+const lastDate = new Date(localStorage.getItem("lastDate")); //getting the last date
+localStorage.setItem("lastDate", currentDate); // creating the last date with the current date.
+
+const daySpace = document.getElementById("days-passed");
+function calcDays (current, last) {
+  const lastDate = new Date(last);
+  const currentDate = new Date(current);
+
+  const timePassed = lastDate.getTime() - currentDate.getTime();
+  const daysPassed = timePassed / (1000 * 3600 * 24);
+
+  return Math.round(daysPassed);
+}
+
+const numDays = calcDays(currentDate, lastDate); 
+if (numDays < 1) {
+    daySpace.innerHTML = "Today";
+}
+else if (numDays == 1) {
+    daySpace.innerHTML = numDays + " day ago"
+}
+else {
+    daySpace.innerHTML = numDays + " days ago"
+}
