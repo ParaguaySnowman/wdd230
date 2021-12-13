4dddeefedd490094afcd911c444c5a3b212cfea3
ParaguaySnowman <parker.b.mortensen@gmail.com>
2021-12-12 18:38:02 -0700

    saving progress to begin testing links

--- final-project/js/common.js
+++ final-project/js/common.js
@@ -1,2 +1,8 @@
+function toggleMenu() {
+    document
+      .getElementsByClassName("navigation")[0]
+  .classList.toggle("responsive");
+  }
+
 //show date when document was last modified
 document.getElementById("date-last-modified").textContent = document.lastModified;
---
+++ final-project/js/weather.js
@@ -0,0 +1,78 @@
+/*
+const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.4152&lon=-111.8315&units=imperial&appid=7501d1d8281e61c575f55a43c59f7e2d"
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const weatherData = jsObject;
+
+    // Current Weather Summary
+
+        //pull data from object
+        const condition = weatherData.current.weather[0].main;
+        const temperature = weatherData.current.temp;
+        const humidity = weatherData.current.humidity;
+        const imgsrc = 'https://openweathermap.org/img/w/' + weatherData.current.weather[0].icon + '.png';
+
+        //fill html
+        document.getElementById("condition").innerHTML = condition;
+        document.getElementById("temperature").innerHTML = Math.round(temperature);
+        document.getElementById("humidity").innerHTML = humidity;
+        document.getElementById(`weather-icon`).setAttribute('src', imgsrc);
+
+    // Three Day Weather Forecast
+    
+    
+        //setup
+        const shortDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        const shortMonths = ["Jan.","Feb.","Mar.","Apr.","May","Jun.","Jul.","Aug.","Sep.","Oct.","Nov.","Dec."];
+    
+
+        //get daily high temperatures
+        let dailyHighTemps = [];
+        dailyHighTemps.push(Math.round(weatherData.daily[1].temp.max));
+        dailyHighTemps.push(Math.round(weatherData.daily[2].temp.max));
+        dailyHighTemps.push(Math.round(weatherData.daily[3].temp.max));
+
+        //get month, date, and day
+        for (let i = 1; i < 4; i++) {
+
+            //get today's date
+            let iFullDate = new Date();
+
+            //increment by the counter
+            iFullDate.setDate(iFullDate.getDate() + i);
+
+            //get the month
+            iMonth = iFullDate.getMonth();
+
+            //get the date
+            iDate = iFullDate.getDate();
+
+            //get the day
+            iDay = iFullDate.getDay();
+
+            //fill HTML
+            document.getElementById("date"+i+"day").innerHTML = shortDays[iDay];
+            document.getElementById("date"+i+"month").innerHTML = shortMonths[iMonth-1];
+            document.getElementById("date"+i+"date").innerHTML = iDate;
+
+            //test
+            document.getElementById("date"+i+"temp").innerHTML = dailyHighTemps[i-1]
+          };
+
+          //weather alerts
+
+          //check if there is a weather alert
+          if(weatherData.hasOwnProperty('alerts')) {
+
+              //get data
+              let alertCategory = weatherData.alerts[0].event;
+              let alertDescription = weatherData.alerts[0].description;
+              
+              //publish alert
+              alert(alertCategory + ":" + alertDescription);
+          };
+  });
