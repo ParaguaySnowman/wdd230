a8e4509947dc8a14fc550a712925a2e29ed835d5
ParaguaySnowman <parker.b.mortensen@gmail.com>
2021-12-12 18:54:47 -0700

    json and js fix for directory

--- final-project/js/directory.js
+++ final-project/js/directory.js
@@ -1,4 +1,4 @@
-const requestURL = 'file:///C:/Users/parke/wdd230/final-project/json/directory.json';
+const requestURL = 'https://paraguaysnowman.github.io/wdd230/final-project/json/directory.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -7,28 +7,31 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
 
-    const prophets = jsonObject['prophets'];/*
+    const businesses = jsonObject['mesa-businesses'];
 
-    for (let i = 0; i < prophets.length; i++ ) {
+    for (let i = 0; i < businesses.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
         let picture = document.createElement('img');
 
-        picture.setAttribute('src', prophets[i].imageurl);
+        picture.setAttribute('src', businesses[i].logo);
         picture.setAttribute('loading', 'lazy');
-        picture.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname);
+        picture.setAttribute('alt', businesses[i].businessName + " logo");
 
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-        p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        h2.textContent = businesses[i].businessName;
+        p1.textContent = 'Phone: ' + businesses[i].contactInfo.phone;
+        p2.textContent = 'Address: ' + businesses[i].contactInfo.address;
+        p3.textContent = 'Website: ' + businesses[i].website;
         
+        card.appendChild(picture);
         card.appendChild(h2);            
         card.appendChild(p1);            
-        card.appendChild(p2);            
-        card.appendChild(picture);
-
+        card.appendChild(p2); 
+        card.appendChild(p3);       
+        
         document.querySelector('div.cards').appendChild(card);
-    }*/    
+    } 
   });
